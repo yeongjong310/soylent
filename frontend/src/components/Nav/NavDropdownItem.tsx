e231462ce3241dbf973f065737b1655e82e5775e
yeongjongkim <tohero21@naver.com>
2021-08-31 12:59:51 +0900

    feat: change DropdownItem top tag to li
    1. Nav의 Item 들을 li태그로 통일
    2. href 조건식 제거 => App에서 dropdownIds로 처리되기 때문

--- frontend/src/components/Nav/NavDropdownItem.tsx
+++ frontend/src/components/Nav/NavDropdownItem.tsx
@@ -1,5 +1,5 @@
-import React, { ReactElement, ReactNode, ComponentType } from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import React, { ReactElement, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 interface dropdownContentProp {
   text: string;
@@ -18,60 +18,50 @@ interface dropdownNavItemType {
   }
 }
 
-export default function DropdownItem({ 
-    href, 
-    text, 
-    dropdownIds,
-    dropdownContent
-  }: dropdownNavItemType): ReactElement {
+export default function DropdownItem({
+  href,
+  text,
+  dropdownIds,
+  dropdownContent
+}: dropdownNavItemType): ReactElement {
+
   return (
-    <>
-      <h2>
-        <button>{text}</button>
-      </h2>
+    <li>
+      <a role='button'>{text}</a>
       {
-        href 
-        ? <NavLink to={href}>{text}</NavLink> // href={href!}
-        : <div className="dropdown">
-            <div className="dropdown__menu_list--image">
-              {
-                dropdownIds?.imageIds.map(dropdownId => {
-                  if (dropdownContent) {
-                    return(
-                      <Link 
-                        to={dropdownContent?.[dropdownId].href} 
-                        className="dropdown__menu--item">
-                          {dropdownContent?.[dropdownId].text}
-                      </Link>
-                    )
-                  }
-                })
-              }
-            </div>
-            <div className="dropdown__menu_list--text">
-              {/* {
-                dropdownIds?.textIds.map(dropdownId=> {
-                  return(
-                    <Link to={dropdownId[navItemId][subMenuId].href} className="dropdown__menu--item">{ subMenu[menuId][subMenuId].text }</Link>
+        <div className="dropdown">
+          <div className="dropdown__menu_list--image">
+            {
+              dropdownIds?.imageIds.map(dropdownId => {
+                if (dropdownContent) {
+                  return (
+                    <Link
+                      to={dropdownContent?.[dropdownId].href}
+                      className="dropdown__menu--item">
+                      {dropdownContent?.[dropdownId].text}
+                    </Link>
                   )
-                })
-              } */}
-              {
-                dropdownIds?.textIds.map(dropdownId => {
-                  if (dropdownContent) {
-                    return(
-                      <Link 
-                        to={dropdownContent?.[dropdownId].href} 
-                        className="dropdown__menu--item">
-                          {dropdownContent?.[dropdownId].text}
-                      </Link>
-                    )
-                  }
-                })
-              }
-            </div>
+                }
+              })
+            }
           </div>
+          <div className="dropdown__menu_list--text">
+            {
+              dropdownIds?.textIds.map(dropdownId => {
+                if (dropdownContent) {
+                  return (
+                    <Link
+                      to={dropdownContent?.[dropdownId].href}
+                      className="dropdown__menu--item">
+                      {dropdownContent?.[dropdownId].text}
+                    </Link>
+                  )
+                }
+              })
+            }
+          </div>
+        </div>
       }
-    </>
+    </li>
   )
 }
