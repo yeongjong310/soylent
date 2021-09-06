8e37720ebd4cf176408a2524cfc6987701d4e783
yeongjongkim <tohero21@naver.com>
2021-09-06 17:57:16 +0900

    feat: implement A11yHidden Component
    issue No: #9
    
    시각적 숨김 처리를 위한 컴포넌트 구현 완료

---
+++ frontend/src/components/A11yHidden/A11yHidden.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { A11yHidden } from './A11yHidden';
+
+export default {
+  title: 'Components/A11yHidden',
+  component: A11yHidden,
+  args: {
+    focusable: false,
+    as: 'a',
+  },
+  argTypes: {
+    className: {
+      control: false
+    },
+  }
+} as ComponentMeta<typeof A11yHidden>;
+
+const Template: ComponentStory<typeof A11yHidden> = (args) => <A11yHidden {...args} />;
+
+export const UnfocasableA11yHidden = Template.bind({});
+UnfocasableA11yHidden.args = {
+  children: 'test',
+};
+
+export const focasableA11yHidden = Template.bind({});
+focasableA11yHidden.args = {
+  children: 'test',
+  focusable: true,
+  href: '#',
+};
