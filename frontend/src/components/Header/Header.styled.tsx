46063572b92adbcda9e19700595b822c4baf0ce8
yeongjong <tohero21@naver.com>
2021-09-09 14:39:55 +0900

    feat: implement fixed Header when scrolling

--- frontend/src/components/Header/Header.styled.tsx
+++ frontend/src/components/Header/Header.styled.tsx
@@ -1,14 +1,34 @@
 import styled from 'styled-components/macro';
-
-export const StyledHeader = styled.header`
-  display: flex;
-  position: fixed;
-  position: sticky;
-  left: 0;
-  right: 0;
-  justify-content: space-between;
-  padding: 0 1.25rem;
-  align-items: center;
+interface styledHeaderType {
+  isOverHeader: () => boolean
+}
+export const StyledHeader = styled.header<styledHeaderType>`
+  /* position: relative; */
+  ${({ isOverHeader }) => {
+    console.log(isOverHeader());
+    return !isOverHeader() 
+    ? 'position: relative;' : 
+    `
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+    ` 
+  }} 
+  
+  z-index: 100;
   height: 3.75rem;
   background: ${({theme}) => theme.colors.white };
+  .header-wrapper {
+    align-items: center;
+    justify-content: space-between;
+    display: flex;
+    height: inherit;
+    padding: 0 1.25rem;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    background: inherit;
+  } 
 `
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { useScroll } from 'hooks/useScroll';
 import { BurgerButton, Logo, Nav, UserContents } from 'components';
 import { StyledHeader } from './Header.styled';
 import Styled from 'styled-components/macro';
@@ -8,31 +9,39 @@ interface HeaderProps {
 }
 
 export default function Header ({ className, ...restProps }: HeaderProps) {
+  const { scrollY } = useScroll();
   const [isOpen, setIsOpen] = useState(false);
+  // const $header = useRef();
+
   const toggleIsOpen = () => {
     setIsOpen(!isOpen);
   }
+
+  const isOverHeader = () => scrollY > 34;
 
   return (
     <StyledHeader
       className={className}
       {...restProps}
-    >
-      {isOpen ? <Nav /> : ''}
-      <BurgerButton isOpen={isOpen} onClick={toggleIsOpen}/>
-      <h1
-        css={`
-          position:absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate3d(-50%, -50%, 0);
-          max-width: 4.75rem;
-          margin: 0;
-        `}
-      >
-        <Logo color='black' />
-      </h1>
-      <UserContents />
+      isOverHeader={isOverHeader}
+    > 
+      <div className="header-wrapper">
+        {isOpen ? <Nav /> : ''}
+        <BurgerButton isOpen={isOpen} onClick={toggleIsOpen}/>
+        <h1
+          css={`
+            position:absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate3d(-50%, -50%, 0);
+            max-width: 4.75rem;
+            margin: 0;
+          `}
+        >
+          <Logo color='black' />
+        </h1>
+        <UserContents />
+      </div>
     </StyledHeader>
   );
 }
